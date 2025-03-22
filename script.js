efa0171368ec8608cbbb705725178fe8252cc741
jaypalande1425 <jaypalande1425@gmail.com>
2025-03-22 14:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("input[type='button']").addEventListener("click", function () {
+        let select = document.getElementById("colorSelect");
+        let selectedOption = select.options[select.selectedIndex];
+        
+        if (selectedOption) {
+            select.remove(select.selectedIndex);
+        }
+    });
+});
